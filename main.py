0ffca0e27f9622c5855412104443253915502415
Sebastián <srvanrell@gmail.com>
2016-01-14 00:07:54 -0300

    get_new_ranking working in a simplest scenario

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ __author__ = 'sebastian'
 import utils
 
 # Fake list of players (0 to 9)
-players_list = [utils.Player(i, "Player%i" % i, rating=100+20*i) for i in range(10)]
+# players_list = [utils.Player(i, "Player%i" % i, rating=100+20*i) for i in range(10)]
+#
+# # printing the list of players
+# for player in players_list:
+#     print player
 
 # Fake list of matches in a single tournament (big number player wins)
 # winner, loser, stage
-matches_list = [[i, j, 'unused'] for i in range(10) for j in range(i)]
-
-# printing the list of players
-for player in players_list:
-    print player
+matches_list = [[i, j, 'q'] for i in range(10) for j in range(i)]
 
 # printing the list of
-for winner, loser, stage in matches_list:
-    print winner, loser
+# for winner, loser, stage in matches_list:
+#     print winner, loser
+#
+#     utils.points_to_assign(winner, loser, players_list)
 
-    utils.points_to_assign(winner, loser, players_list)
+# Fake ranking list
+# player_id, rating
+old_ranking = [[i, 1000+24*i] for i in range(10)]
+
+print old_ranking
+
+new_ranking = utils.get_new_ranking(old_ranking, matches_list)
+
+print new_ranking
 
--- utils.py
+++ utils.py
@@ -28,58 +28,97 @@ class Match:
         self.winner = -1
         self.looser = -1
 
+##############################
+# Tables to assign points
+##############################
 
-def points_to_assign(nid_winner, nid_loser, players_table):
-    """Return points to assign to each player given """
-    points_diff = players_table[nid_winner].rating - players_table[nid_loser].rating
+# Expected result table
+# TODO, falta chequear
+# difference, points to winner, points to loser
+expected_result = [[24, 9, 9],
+                   [49, 8, 8],
+                   [99, 7, 7],
+                   [149, 6, 6],
+                   [199, 5, 5],
+                   [299, 4, 4],
+                   [399, 3, 3],
+                   [499, 2, 2],
+                   [999, 1, 1]]
 
-    # tables to assign points
+# TODO, falta chequear
+# difference (should be negative), points to winner, points to loser
+unexpected_result = [[24, 10, 9],
+                     [49, 11, 10],
+                     [99, 13, 11],
+                     [149, 15, 12],
+                     [199, 18, 14],
+                     [299, 21, 16],
+                     [399, 24, 18],
+                     [499, 28, 21],
+                     [999, 32, 25]]
 
-    # Expected result table
-    # difference, points to winner, points to loser
-    expected_result = [[24, 9, 9],
-                       [49, 8, 8],
-                       [99, 7, 7],
-                       [149, 6, 6],
-                       [199, 5, 5],
-                       [299, 4, 4],
-                       [399, 3, 3],
-                       [499, 2, 2],
-                       [999, 1, 1]]
+# points to be assigned by round
+# TODO, falta chequear
+round_points = {'z': 1,
+                'o': 2,
+                'q': 4,
+                's': 8,
+                '2': 10,
+                '1': 12}
 
-    # difference, points to winner, points to loser
-    unexpected_result = [[24, 10, 9],
-                         [49, 11, 10],
-                         [99, 13, 11],
-                         [149, 15, 12],
-                         [199, 18, 14],
-                         [299, 21, 16],
-                         [399, 24, 18],
-                         [499, 28, 21],
-                         [999, 32, 25]]
+
+def points_to_assign(rating_winner, rating_loser):
+    """Return points to assign to each player rating given """
+    rating_diff = rating_winner - rating_loser
 
     assignation_table = expected_result
-    if points_diff < 0:
-        print "expected result"
+    if rating_diff < 0:
+        rating_diff *= -1.0
         assignation_table = unexpected_result
-    # else:
-    #     print "unexpected result"
 
     i = 0
-    while points_diff > assignation_table[i][0]:
+    while rating_diff > assignation_table[i][0]:
         i += 1
 
     points_to_winner = assignation_table[i][1]
     points_to_loser = assignation_table[i][2]
-    print "diff:%d, to_winner:%d, to_loser:%d" % (points_diff, points_to_winner, points_to_loser)
+    print "diff:%d, to_winner:%d, to_loser:%d" % (rating_diff, points_to_winner, points_to_loser)
 
+    return [points_to_winner, points_to_loser]
+
+# TODO ranking model, get_rating, update_rating
+def get_rating(player_id, ranking):
+    return ranking[player_id]
 
 def get_new_ranking(old_ranking, matches_list):
-    new_ranking = old_ranking + new_players
+    # TODO make a better way to copy a list, maybe the db saves me
+    new_ranking = [list(item) for item in old_ranking]
+    # TODO add new players
 
-    for match in matches_list:
+    for winner, loser, unused in matches_list:
+        # TODO change the way to read old ratings
+        [to_winner, to_loser] = points_to_assign(old_ranking[winner][1], old_ranking[loser][1])
+        # TODO change the way to read new ratings
+        new_ranking[winner][1] += to_winner
+        new_ranking[loser][1] -= to_loser
 
-        [points_to_winner, points_to_loser] = points_to_assign(winner_rating, loser_rating)
-        new_ranking.update_points(winner, points_to_winner)
-        new_ranking.update_points(loser, points_to_loser)
+    # TODO add points per best round reached
+    best_round_to_assign = {}
+
+    for winner, loser, round_match in matches_list:
+        if best_round_to_assign.get(winner):
+            if best_round_to_assign.get(winner) < round_points[round_match]:
+                best_round_to_assign[winner] = round_points[round_match]
+        else:
+            best_round_to_assign[winner] = round_points[round_match]
+        if best_round_to_assign.get(loser):
+            if best_round_to_assign.get(loser) < round_points[round_match]:
+                best_round_to_assign[loser] = round_points[round_match]
+        else:
+            best_round_to_assign[loser] = round_points[round_match]
+
+    for player_id in best_round_to_assign:
+        new_ranking[player_id][1] += best_round_to_assign[player_id]
+
+    return new_ranking
 
