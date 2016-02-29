55e063d22ddf4f2e0cfbbfed2b51de819d874443
Sebastián <srvanrell@gmail.com>
2016-02-29 09:34:12 -0300

    Adjust to points per round points

---
+++ test.py
@@ -0,0 +1,21 @@
+__author__ = 'sebastian'
+# -*- coding: utf-8 -*-
+
+# Codigo para inicializar dos orillas
+
+import utils
+
+tournament1_filename = "Liga Dos Orillas - Torneos Disputados 2016 - 1er Torneo 2016.csv"
+tournament2_filename = "Liga Dos Orillas - Torneos Disputados 2016 - 2do Torneo 2016.csv"
+
+tournament1 = utils.load_tournament_csv(tournament1_filename)
+tournament2 = utils.load_tournament_csv(tournament2_filename)
+
+allplayers = list(set(tournament1["players"]).union(set(tournament2["players"])))
+
+players = [[num+1, player, "Asociación"] for num, player in enumerate(allplayers)]
+ratings = [[pid, 1000, name] for pid, name, asociation in players]
+
+utils.save_csv("jugadores_dos_orillas.csv", ["player_id", "name", "association"], players)
+utils.save_csv("ranking_inicial_dos_orillas.csv", ["player_id", "rating", "name"], ratings)
+
