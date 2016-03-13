74822ce24c7327345059063dd16c09a7b8d2003f
Sebastián <srvanrell@gmail.com>
2016-03-13 01:27:47 -0300

    Models improved. Addition of new players

--- models.py
+++ models.py
@@ -31,25 +31,31 @@ class PlayersList:
     def add_player(self, player):
         if player.pid not in self.players:
             self.players[player.pid] = player
-
+        else:
+            print "WARNING: Already exists a player for that pid. Check:", str(player)
+    
+    def add_new_player(self, name):
+        pid = 0
+        while pid in self.players:
+            pid += 1
+        self.add_player(Player(pid, name))
+    
     # def __contains__(self, ):
-
 
     def get_pid(self, name):
         for player in self.players.itervalues():
             if name == player.name:
                 return player.pid
-        print "Jugador desconocido"
+        print "WARNING: Unknown player:", name
         return None
 
-    # TODO add to list and load from list functions
     def to_list(self):
-        # Todo return a list to be saved as csv
-        return self.players
+        players_list = [[p.pid, p.name, p.association, p.city] for p in self.players.itervalues()]
+        return players_list
 
-    def load_list(self):
-        # Load from a list saved as csv
-        return self.get_player(1)
+    def load_list(self, players_list):
+        for pid, name, association, city in players_list:
+            self.add_player(Player(pid, name, association, city))
 
 
 class RankingEntry:
@@ -57,20 +63,28 @@ class RankingEntry:
         self.pid = pid
         self.rating = rating
         self.bonus = bonus
+        
+    def __repr__(self):
+        return ";".join([str(self.pid), str(self.rating), str(self.bonus)])
 
 
 class Ranking:
-    # TODO ranking model and managment
     def __init__(self):
         self.ranking = {}
         self.date = ""
         self.tournament = ""
 
     def add_entry(self, pid, rating, bonus):
-        self.ranking[pid] = RankingEntry(pid, rating, bonus)
+        if pid not in self.ranking:
+            self.ranking[pid] = RankingEntry(pid, rating, bonus)
+        else:
+            print "WARNING: Already exists an entry for pid:", pid
 
     def get_entry(self, pid):
         return self.ranking[pid]
+        
+    def __getitem__(self, pid):
+        return self.get_entry(pid)
 
     def __repr__(self):
         return "\n".join(str(self.get_entry(re)) for re in self.ranking)
--- test.py
+++ test.py
@@ -4,18 +4,41 @@ __author__ = 'sebastian'
 # Codigo para inicializar dos orillas
 
 import utils
+import models
 
-tournament1_filename = "Liga Dos Orillas - Torneos Disputados 2016 - 1er Torneo 2016.csv"
-tournament2_filename = "Liga Dos Orillas - Torneos Disputados 2016 - 2do Torneo 2016.csv"
+players_info_filename = "jugadores_dos_orillas.csv"
 
-tournament1 = utils.load_tournament_csv(tournament1_filename)
-tournament2 = utils.load_tournament_csv(tournament2_filename)
+tournament_filenames = ["Liga Dos Orillas 2016 - Ranking Mayores - 1er Torneo 2016.csv",
+                        "Liga Dos Orillas 2016 - Ranking Mayores - 2do Torneo 2016.csv",
+                        "Liga Dos Orillas 2016 - Ranking Mayores - 3er Torneo 2016.csv"
+                        ]
 
-allplayers = list(set(tournament1["players"]).union(set(tournament2["players"])))
+for tournament_filename in tournament_filenames:
+    # Loading tournament info
+    tournament = utils.load_tournament_csv(tournament_filename)
 
-players = [[num+1, player, "Asociación"] for num, player in enumerate(allplayers)]
-ratings = [[pid, 1000, name] for pid, name, asociation in players]
+    # Loading and completing the players list
+    players = models.PlayersList()
+    players.load_list(utils.load_csv(players_info_filename))
 
-utils.save_csv("jugadores_dos_orillas.csv", ["player_id", "name", "association"], players)
-utils.save_csv("ranking_inicial_dos_orillas.csv", ["player_id", "rating", "name"], ratings)
+    for name in tournament["players"]:
+        if players.get_pid(name) is None:
+            players.add_new_player(name)
+
+    utils.save_csv(players_info_filename, 
+                   ["PID", "Nombre", "Asociación", "Ciudad"], 
+                   players.to_list())
+
+
+#tournament2_filename = "Liga Dos Orillas - Torneos Disputados 2016 - 2do Torneo 2016.csv"
+#tournament2 = utils.load_tournament_csv(tournament2_filename)
+
+
+#allplayers = list(set(tournament1["players"]).union(set(tournament2["players"])))
+
+#players = [[num+1, player, "Asociación"] for num, player in enumerate(allplayers)]
+#ratings = [[pid, 1000, name] for pid, name, asociation in players]
+
+#utils.save_csv("jugadores_dos_orillas.csv", ["player_id", "name", "association"], players)
+#utils.save_csv("ranking_inicial_dos_orillas.csv", ["player_id", "rating", "name"], ratings)
 
