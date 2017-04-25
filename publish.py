f6e23ef2d5a5765932e0a9cd507e38ffa85cce5a
Sebastián <srvanrell@gmail.com>
2017-04-25 14:47:17 -0300

    Publish copy log sheet

--- publish.py
+++ publish.py
@@ -4,17 +4,19 @@ from utils import cfg
 
 __author__ = 'sebastian'
 
-##########################################
-# Script to run after compute_all.py
+###################################################
+# Script to run after compute_single_tournament.py
 # Input: xlsx tournaments database
+#        xlsx rankings database
+#        xlsx log file
 #        config.yaml
 # Output: temp xlsx to publish a single ranking
-##########################################
+###################################################
 
 tournaments_xlsx = cfg["io"]["data_folder"] + cfg["io"]["tournaments_filename"]
 rankings_xlsx = cfg["io"]["data_folder"] + cfg["io"]["rankings_filename"]
-temp_xlsx = cfg["io"]["data_folder"] + "temp.xlsx"
-histories_xlsx = temp_xlsx
+log_xlsx = cfg["io"]["data_folder"] + cfg["io"]["log_filename"]
+output_xlsx = cfg["io"]["data_folder"] + "Torneo NN para publicar.xlsx"
 
 # Listing tournament sheetnames by increasing date
 tournament_sheetnames = utils.get_sheetnames_by_date(tournaments_xlsx, cfg["sheetname"]["tournaments_key"])
@@ -30,6 +32,7 @@ for tid, tournament_sheetname in enumerate(tournament_sheetnames, start=1):
     print("\n%d\t->\t%s" % (tid, tournament_sheetname))
 tid = int(input("Enter the tournament id to publish (look above):\n"))
 tournament_sheetname = tournament_sheetnames[tid-1]
+output_xlsx = output_xlsx.replace("NN", "%d" % tid)
 
 # Loading tournament info
 tournament = utils.load_tournament_xlsx(tournaments_xlsx, tournament_sheetname)
@@ -39,39 +42,26 @@ ranking = utils.load_ranking_sheet(rankings_xlsx, tournament_sheetname.replace(
 ranking.tid = tid
 
 # Saving new ranking
-utils.publish_rating_sheet(temp_xlsx, tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-                                                                   cfg["labels"]["Rating Points"]),
+utils.publish_rating_sheet(output_xlsx, tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
+                                                                     cfg["labels"]["Rating Points"]),
                            ranking, players, True)
 
 # Saving new ranking
-utils.publish_championship_sheet(temp_xlsx, tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-                                                                         "Campeonato"),
+utils.publish_championship_sheet(output_xlsx, tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
+                                                                           "Campeonato"),
                                  ranking, players, True)
 
 # Saving complete histories of players
-utils.publish_histories_sheet(histories_xlsx, "Historiales", players, tournament_sheetnames, True)
+utils.publish_histories_sheet(output_xlsx, "Historiales", players, tournament_sheetnames, True)
 
-# TODO make a copy of point log sheets
-# # Saving points assigned in each match
-# points_log_to_save = [[players[winner_pid].name, players[loser_pid].name, winner_points, loser_points]
-#                       for winner_pid, loser_pid, winner_points, loser_points in assigned_points_per_match]
-#
-# utils.save_sheet_workbook(log_xlsx,
-#                           tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-#                                                        cfg["sheetname"]["rating_details_key"]),
-#                           [cfg["labels"][key] for key in ["Winner", "Loser", "Winner Points", "Loser Points"]],
-#                           points_log_to_save, True)
-#
-# # Saving points assigned per best round reached and for participation
-# points_log_to_save = [[players[pid].name, points, best_round, category] for pid, points, best_round, category
-#                       in assigned_points_per_best_round]
-# participation_points_log_to_save = [[players[pid].name, points, cfg["labels"]["Participation Points"], ""]
-#                                     for pid, points in assigned_participation_points]
-#
-# utils.save_sheet_workbook(log_xlsx,
-#                           tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-#                                                        cfg["sheetname"]["bonus_details_key"]),
-#                           [cfg["labels"][key] for key in ["Player", "Bonus Points", "Best Round", "Category"]],
-#                           # points_log_to_save, True)
-#                           points_log_to_save + participation_points_log_to_save, True)
+# Saving points assigned in each match
+rating_details_sheetname = tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
+                                                        cfg["sheetname"]["rating_details_key"])
+rating_log_saved = utils.load_sheet_workbook(log_xlsx, rating_details_sheetname, first_row=0)
+utils.save_sheet_workbook(output_xlsx, rating_details_sheetname, rating_log_saved[0], rating_log_saved[1:], True)
 
+# Saving points assigned per best round reached and for participation
+bonus_details_sheetname = tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
+                                                       cfg["sheetname"]["bonus_details_key"])
+bonus_log_saved = utils.load_sheet_workbook(log_xlsx, bonus_details_sheetname, first_row=0)
+utils.save_sheet_workbook(output_xlsx, bonus_details_sheetname, bonus_log_saved[0], bonus_log_saved[1:], True)
