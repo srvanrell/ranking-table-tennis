b1b91538454d5ade7b7d4f09fe57b5803cf2d429
srvanrell <srvanrell@gmail.com>
2017-11-28 13:53:53 -0300

    Add initial setup for the package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ranking_table_tennis',
+      version='0.1',
+      description='A ranking table tennis system',
+      url='http://github.com/srvanrell/ranking-table-tennis',
+      author='Sebastián Vanrell',
+      author_email='srvanrell@gmail.com',
+      license='MIT',
+      packages=['ranking_table_tennis'],
+      scripts=['bin/preprocess']
+      zip_safe=False)
