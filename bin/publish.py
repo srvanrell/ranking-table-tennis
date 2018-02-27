d486daf1f4f72285e4406ff732484b9d6eed9197
srvanrell <srvanrell@gmail.com>
2018-02-27 13:42:42 -0300

    Testing publishing initial rating

--- bin/publish.py
+++ bin/publish.py
@@ -58,3 +58,6 @@ utils.publish_details_sheets(tournament_sheetname, ranking, upload=upload)
 
 # Saving complete histories of players
 utils.publish_histories_sheet(ranking, players, tournament_sheetnames, upload=upload)
+
+# Testing publshing initial_ranking
+utils.publish_rating_sheet(tournament_sheetname, initial_ranking, players, initial_ranking, upload=upload)
