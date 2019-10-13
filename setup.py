52615029d882f0e86a5eb31d4f762748ca60f570
srvanrell <srvanrell@gmail.com>
2019-10-13 19:05:10 -0300

    Change minor version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PostInstallCommand(install):
 
 
 setup(name='ranking_table_tennis',
-      version='2019.4.3',
+      version='2019.4.4',
       description='A ranking table tennis system',
       long_description=readme(),
       long_description_content_type="text/markdown",
