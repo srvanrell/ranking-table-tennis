d894287e6187c928ac14bed5a4e49f6a98e94dac
srvanrell <srvanrell@gmail.com>
2020-04-02 21:29:15 -0300

    Prepare for a new version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PostInstallCommand(install):
 
 
 setup(name='ranking_table_tennis',
-      version='2020.4.2',
+      version='2020.4.3',
       description='A ranking table tennis system',
       long_description=readme(),
       long_description_content_type="text/markdown",
