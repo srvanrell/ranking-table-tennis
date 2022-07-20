89edd78e90188de96f7a2313850df673a59c7acf
sebastian <srvanrell@gmail.com>
2022-07-19 23:43:49 -0300

    Scripts for steps has been refactored as functions modules

--- ranking_table_tennis/preprocess.py
+++ ranking_table_tennis/preprocess.py
@@ -17,86 +17,92 @@ __author__ = "sebastian"
 # and saves the result into the same xlsx
 ##########################################
 
-xlsx_file = cfg.io.data_folder + cfg.io.tournaments_filename
 
-retrieve = input("Do you want to retrieve online sheet [Y/n]? (press Enter to continue)\n")
-if retrieve.lower() != "n":
-    print("Downloading and saving %s\n" % xlsx_file)
-    request.urlretrieve(cfg.io.tournaments_gdrive, xlsx_file)
+def main():
+    xlsx_file = cfg.io.data_folder + cfg.io.tournaments_filename
 
-# Loading all tournament data
-tournaments = utils.load_tournaments_sheets()
+    retrieve = input("Do you want to retrieve online sheet [Y/n]? (press Enter to continue)\n")
+    if retrieve.lower() != "n":
+        print("Downloading and saving %s\n" % xlsx_file)
+        request.urlretrieve(cfg.io.tournaments_gdrive, xlsx_file)
 
-# Loading players list
-players = utils.load_players_sheet()
+    # Loading all tournament data
+    tournaments = utils.load_tournaments_sheets()
 
-# Loading initial ranking
-rankings = utils.load_initial_ranking_sheet()
-initial_tid = cfg.aux.initial_tid
+    # Loading players list
+    players = utils.load_players_sheet()
 
-# Loading temp ranking and players. It will be deleted after a successful preprocessing
-players_temp, ranking_temp = utils.load_temp_players_ranking()
+    # Loading initial ranking
+    rankings = utils.load_initial_ranking_sheet()
+    initial_tid = cfg.aux.initial_tid
 
-for tid in tournaments:
-    print("==", tid, "==")
+    # Loading temp ranking and players. It will be deleted after a successful preprocessing
+    players_temp, ranking_temp = utils.load_temp_players_ranking()
 
-    for name in tournaments.get_players_names(tid):
-        unknown_player = False
-        if players.get_pid(name) is None:
-            if players_temp.get_pid(name) is None:
-                unknown_player = True
-                association = input("\nEnter the affiliation of %s: (optional field)\n" % name)
-                city = input("\nEnter the city of %s: (optional field)\n" % name)
-                # Assign a pid for the new given player and add it to the list
-                players.add_new_player(name, association, city)
-                # Save a temp player to resume preprocessing, if necessary
-                players_temp.add_player(players[players.get_pid(name)])
-            else:
-                print(">>>>\tUNCOMPLETE preprocessing detected. Resuming...")
-                players.add_player(players_temp[players_temp.get_pid(name)])
-            print("\n", players[players.get_pid(name)], "\n")
+    for tid in tournaments:
+        print("==", tid, "==")
 
-        pid = players.get_pid(name)
+        for name in tournaments.get_players_names(tid):
+            unknown_player = False
+            if players.get_pid(name) is None:
+                if players_temp.get_pid(name) is None:
+                    unknown_player = True
+                    association = input("\nEnter the affiliation of %s: (optional field)\n" % name)
+                    city = input("\nEnter the city of %s: (optional field)\n" % name)
+                    # Assign a pid for the new given player and add it to the list
+                    players.add_new_player(name, association, city)
+                    # Save a temp player to resume preprocessing, if necessary
+                    players_temp.add_player(players[players.get_pid(name)])
+                else:
+                    print(">>>>\tUNCOMPLETE preprocessing detected. Resuming...")
+                    players.add_player(players_temp[players_temp.get_pid(name)])
+                print("\n", players[players.get_pid(name)], "\n")
 
-        # Category will be asin
-        if rankings[initial_tid, pid] is None:
-            if ranking_temp[initial_tid, pid] is None:
-                unknown_player = True
-                text_to_show = f"\nEnter the initial rating points for {name}"
-                text_to_show += " (category will be auto-assigned):\n"
-                initial_rating = int(input(text_to_show))
-                rankings.add_new_entry(initial_tid, pid, initial_rating)
-                # Save a temp ranking of the player to resume preprocessing, if necessary
-                ranking_temp.add_entry(rankings[initial_tid, pid])
-            else:
-                print(">>>>\tUNCOMPLETE preprocessing detected. Resuming...")
-                rankings.add_entry(ranking_temp[initial_tid, pid])
+            pid = players.get_pid(name)
 
-            print(
-                "\n",
-                rankings[initial_tid, pid][["tid", "pid", "rating", "category"]],
-                players[pid]["name"],
-                "\n",
-            )
+            # Category will be asin
+            if rankings[initial_tid, pid] is None:
+                if ranking_temp[initial_tid, pid] is None:
+                    unknown_player = True
+                    text_to_show = f"\nEnter the initial rating points for {name}"
+                    text_to_show += " (category will be auto-assigned):\n"
+                    initial_rating = int(input(text_to_show))
+                    rankings.add_new_entry(initial_tid, pid, initial_rating)
+                    # Save a temp ranking of the player to resume preprocessing, if necessary
+                    ranking_temp.add_entry(rankings[initial_tid, pid])
+                else:
+                    print(">>>>\tUNCOMPLETE preprocessing detected. Resuming...")
+                    rankings.add_entry(ranking_temp[initial_tid, pid])
 
-        if unknown_player:
-            retrieve = input(
-                "press Enter to continue or Kill this process to forget last player data\n"
-            )
-            utils.save_temp_players_ranking(players_temp, ranking_temp)
+                print(
+                    "\n",
+                    rankings[initial_tid, pid][["tid", "pid", "rating", "category"]],
+                    players[pid]["name"],
+                    "\n",
+                )
 
-# Update the online version
-answer = input("\nDo you want to update online sheets [Y/n]? (press Enter to continue)\n")
-upload = answer.lower() != "n"
+            if unknown_player:
+                retrieve = input(
+                    "press Enter to continue or Kill this process to forget last player data\n"
+                )
+                utils.save_temp_players_ranking(players_temp, ranking_temp)
 
-# Saving complete list of players, including new ones
-utils.save_players_sheet(players, upload=upload)
+    # Update the online version
+    answer = input("\nDo you want to update online sheets [Y/n]? (press Enter to continue)\n")
+    upload = answer.lower() != "n"
 
-# # Saving initial rankings for all known players
-rankings.update_categories()
-utils.save_ranking_sheet(initial_tid, tournaments, rankings, players, upload=upload)
+    # Saving complete list of players, including new ones
+    utils.save_players_sheet(players, upload=upload)
 
-# Remove temp files after a successful preprocessing
-utils.remove_temp_players_ranking()
+    # # Saving initial rankings for all known players
+    rankings.update_categories()
+    utils.save_ranking_sheet(initial_tid, tournaments, rankings, players, upload=upload)
 
-utils.save_to_pickle(players=players, tournaments=tournaments)
+    # Remove temp files after a successful preprocessing
+    utils.remove_temp_players_ranking()
+
+    utils.save_to_pickle(players=players, tournaments=tournaments)
+
+
+if __name__ == "__main__":
+    main()
