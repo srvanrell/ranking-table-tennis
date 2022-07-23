6ddd5012dac428af5f1d3d1ffe37e5c370983bcc
sebastian <srvanrell@gmail.com>
2022-07-22 22:27:57 -0300

    Really improve preprocess and compute rankings tests

--- tests/test_compute_rankings.py
+++ tests/test_compute_rankings.py
@@ -1,58 +1,40 @@
-import ranking_table_tennis.compute_rankings
+import pytest
+from ranking_table_tennis import preprocess, compute_rankings
 from ranking_table_tennis.configs import cfg
 import pickle
 import os
 from pandas.testing import assert_frame_equal
 
 
-def test_compute_rankings_outputs_players_and_histories():
-    # Run complete compute_rankings unattended
-    ranking_table_tennis.compute_rankings.main()
+@pytest.fixture(scope="module", autouse=True)
+def my_fixture():
+    """To be run once before all tests"""
+    preprocess.main()
+    compute_rankings.main()
 
+
+def test_compute_rankings_outputs_players_and_histories(players_df, history_df):
     # Load output
     output_filename = os.path.join(cfg.io.data_folder, "players.pk")
     players_output = pickle.load(open(output_filename, "rb"))
 
-    # Load expected output
-    expected_path = os.path.join(os.path.dirname(__file__), "data_up_to_S2022T04")
-    expected_filename = os.path.join(expected_path, "players.pk")
-    players_expected = pickle.load(open(expected_filename, "rb"))
-
-    assert_frame_equal(players_output.players_df, players_expected.players_df)
-    assert_frame_equal(players_output.history_df, players_expected.history_df)
+    assert_frame_equal(players_output.players_df, players_df)
+    assert_frame_equal(players_output.history_df, history_df)
 
 
-def test_compute_rankings_outputs_tournaments():
-    # Run complete compute_rankings unattended
-    # ranking_table_tennis.compute_rankings.main()
-
+def test_compute_rankings_outputs_tournaments(tournaments_df):
     # Load output
     output_filename = os.path.join(cfg.io.data_folder, "tournaments.pk")
     tournaments_output = pickle.load(open(output_filename, "rb"))
 
-    # Load expected output
-    expected_path = os.path.join(os.path.dirname(__file__), "data_up_to_S2022T04")
-    expected_filename = os.path.join(expected_path, "tournaments.pk")
-    tournaments_expected = pickle.load(open(expected_filename, "rb"))
-
-    assert_frame_equal(tournaments_output.tournaments_df, tournaments_expected.tournaments_df)
+    assert_frame_equal(tournaments_output.tournaments_df, tournaments_df)
 
 
-def test_compute_rankings_outputs_rankings():
-    # Run complete compute_rankings unattended
-    # ranking_table_tennis.compute_rankings.main()
-
+def test_compute_rankings_outputs_rankings(ranking_df, rating_details_df, championship_details_df):
     # Load output
     output_filename = os.path.join(cfg.io.data_folder, "rankings.pk")
     rankings_output = pickle.load(open(output_filename, "rb"))
 
-    # Load expected output
-    expected_path = os.path.join(os.path.dirname(__file__), "data_up_to_S2022T04")
-    expected_filename = os.path.join(expected_path, "rankings.pk")
-    rankings_expected = pickle.load(open(expected_filename, "rb"))
-
-    assert_frame_equal(rankings_output.ranking_df, rankings_expected.ranking_df)
-    assert_frame_equal(rankings_output.rating_details_df, rankings_expected.rating_details_df)
-    assert_frame_equal(
-        rankings_output.championship_details_df, rankings_expected.championship_details_df
-    )
+    assert_frame_equal(rankings_output.ranking_df, ranking_df)
+    assert_frame_equal(rankings_output.rating_details_df, rating_details_df)
+    assert_frame_equal(rankings_output.championship_details_df, championship_details_df)
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,41 +1,36 @@
-import ranking_table_tennis.preprocess
+import pytest
+from ranking_table_tennis import preprocess
 from ranking_table_tennis.configs import cfg
+from ranking_table_tennis import utils
 import pickle
 import os
 from pandas.testing import assert_frame_equal
 
 
-def test_preprocess_outputs_players_and_histories():
-    # Run complete preprocess unattended and offline
-    ranking_table_tennis.preprocess.main()
+@pytest.fixture(scope="module", autouse=True)
+def my_fixture():
+    """To be run once before all tests"""
+    preprocess.main()
 
+
+def test_preprocess_outputs_players_and_histories(players_df):
     # Load output from preprocess
     output_filename = os.path.join(cfg.io.data_folder, "players.pk")
     players_output = pickle.load(open(output_filename, "rb"))
 
-    # Load expected output
-    expected_path = os.path.join(os.path.dirname(__file__), "data_up_to_S2022T04")
-    expected_filename = os.path.join(expected_path, "players.pk")
-    players_expected = pickle.load(open(expected_filename, "rb"))
-
-    assert_frame_equal(players_output.players_df, players_expected.players_df)
+    assert_frame_equal(players_output.players_df, players_df)
     assert players_output.history_df.empty
 
 
-def test_preprocess_outputs_tournaments():
-    # Run complete preprocess unattended and offline
-    # ranking_table_tennis.preprocess.main()
-
+def test_preprocess_outputs_tournaments(tournaments_df):
     # Load output from preprocess
-    output_filename = os.path.join(cfg.io.data_folder, "tournaments.pk")
-    tournaments_output = pickle.load(open(output_filename, "rb"))
-
-    # Load expected output
-    expected_path = os.path.join(os.path.dirname(__file__), "data_up_to_S2022T04")
-    expected_filename = os.path.join(expected_path, "tournaments.pk")
-    tournaments_expected = pickle.load(open(expected_filename, "rb"))
+    tournaments_output = utils.load_from_pickle(cfg.io.tournaments_pickle)
 
     # winner and loser Players ID are not assignated during preprocessing
-    tournaments_expected.tournaments_df.winner_pid = None
-    tournaments_expected.tournaments_df.loser_pid = None
-    assert_frame_equal(tournaments_output.tournaments_df, tournaments_expected.tournaments_df)
+    tournaments_df = tournaments_df.copy()
+    tournaments_df.winner_pid = None
+    tournaments_df.loser_pid = None
+
+    print(tournaments_df.head())
+    print(tournaments_output.tournaments_df.head())
+    assert_frame_equal(tournaments_output.tournaments_df, tournaments_df)
