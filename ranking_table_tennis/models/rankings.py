Fix no future stats computation

ranking_table_tennis/models/rankings.py
@@ -509,16 +509,23 @@ class Rankings:
 
         return pid_count
 
-    def get_statistics(self) -> pd.DataFrame:
+    def get_statistics(self, tid) -> pd.DataFrame:
         """
         Return a DataFrame that summarizes total and in each category participations:
         - the number of players that have participated up to tournament(tid)
         - the number of players that have participated in tournament(tid)
+
+        tid: ID of max tournament to consider
         """
+        # TIDs to consider in statistic computation
+        tids_to_consider = self.ranking_df.tid <= tid
+
         # stats by category
         columns = ["tid"] + self.cum_points_cat_columns() + self.points_cat_columns()
         stats_cat = (
-            self.ranking_df.loc[:, columns].groupby("tid").apply(lambda df: (df > 0).sum(axis=0))
+            self.ranking_df.loc[tids_to_consider, columns]
+            .groupby("tid")
+            .apply(lambda df: (df > 0).sum(axis=0))
         )
         stats_cat.rename(
             lambda col: col.replace("points", "participation"), axis="columns", inplace=True
@@ -527,7 +534,7 @@ class Rankings:
         # total stats cumulated. multi category players on a tournament are counted once
         columns = ["tid", "pid"] + self.cum_points_cat_columns()
         cum_participation_total = (
-            self.ranking_df.loc[:, columns]
+            self.ranking_df.loc[tids_to_consider, columns]
             .groupby("tid")
             .apply(self._count_unique_pids, self.cum_points_cat_columns())
         )