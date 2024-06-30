c325484384b069e4e6db9e223f398f52ba234c69
sebastian <srvanrell@gmail.com>
2024-06-30 17:22:55 -0300

    fix github stopping workflow

--- ranking_table_tennis/helpers/github.py
+++ ranking_table_tennis/helpers/github.py
@@ -18,10 +18,13 @@ def no_updates_stop_workflow(spreadsheet_id, max_days_since_last_update=1):
     GITHUB_OUTPUT_VAR_NAME = "GITHUB_OUTPUT"
     var_value = f"stop_workflow={str(stop_workflow).lower()}"  # true or false
 
-    logger.debug("Set env var '%s': '%s'", GITHUB_OUTPUT_VAR_NAME, var_value)
     ghoutput = os.getenv(GITHUB_OUTPUT_VAR_NAME)
-    with open(ghoutput, "a") as fh:
-        fh.write(var_value)
+    if ghoutput is not None:
+        logger.debug("Set env var '%s': '%s'", GITHUB_OUTPUT_VAR_NAME, var_value)
+
+        with open(ghoutput, "a") as fh:
+            fh.write(var_value)
 
     if stop_workflow:
+        logger.info("Stopping preprocess!")
         exit(0)
