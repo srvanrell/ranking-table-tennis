9fe817131ab3edf23221151e1e1579a16265426e
Sebastian <srvanrell@gmail.com>
2025-04-02 14:09:02 -0300

    add tying annotations

--- ranking_table_tennis/configs.py
+++ ranking_table_tennis/configs.py
@@ -1,6 +1,7 @@
 import glob
 import logging
 import os
+from typing import List
 
 import hydra
 import pandas as pd
@@ -10,39 +11,6 @@ USER_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config")
 AVAILABLE_CONFIGS = glob.glob(os.path.join(USER_CONFIG_PATH, "config_*_*.yaml"))
 
 logger = logging.getLogger(__name__)
-
-
-class ConfigManager:
-    _current_config = None
-    _available_configs = None
-
-    def __init__(self) -> None:
-        pass
-
-    def initialize(self):
-        if ConfigManager._available_configs is None:
-            self.set_available_configs()
-
-    def set_available_configs(self):
-        ConfigManager._available_configs = []
-        for path in sorted(AVAILABLE_CONFIGS, reverse=True):
-            ConfigManager._available_configs.append(Configuration(path))
-            logger.debug("~ Available %s", ConfigManager._available_configs[-1])
-
-    def get_valid_config(self, date):
-        self.initialize()
-        for conf in ConfigManager._available_configs:
-            if conf.start_valid_date <= date <= conf.end_valid_date:
-                return conf.get_config()
-
-    @property
-    def current_config(self):
-        return ConfigManager._current_config
-
-    def set_current_config(self, date):
-        ConfigManager._current_config = self.get_valid_config(date)
-        if not os.path.exists(ConfigManager._current_config.io.data_folder):
-            os.mkdir(ConfigManager._current_config.io.data_folder)
 
 
 class Configuration:
@@ -58,13 +26,13 @@ class Configuration:
     def __str__(self) -> str:
         return f"Configuration(start={self.start_valid_date}, end={self.end_valid_date})"
 
-    def _set_dates(self):
+    def _set_dates(self) -> None:
         if "_" in self._config_name:
             _, start_date, end_date = self._config_name.split("_")
             self.start_valid_date = start_date
             self.end_valid_date = end_date
 
-    def get_config(self):
+    def get_config(self) -> OmegaConf:
         """Load config from the config_path."""
         if self.dict_cfg is None:
             logger.debug("~ Config directory: '%s'", os.path.abspath(self._config_path))
@@ -75,7 +43,7 @@ class Configuration:
 
         return self.dict_cfg
 
-    def _load_base_config(self):
+    def _load_base_config(self) -> OmegaConf:
         config_dir = os.path.abspath(self._config_path)
         with hydra.initialize_config_dir(
             version_base=None, config_dir=config_dir, job_name="rtt_app"
@@ -83,7 +51,7 @@ class Configuration:
             logger.debug("~ Loading '%s' @ '%s'", self._config_name, config_dir)
             return hydra.compose(config_name=self._config_name)
 
-    def _load_tables_config(self, base_cfg):
+    def _load_tables_config(self, base_cfg: OmegaConf) -> OmegaConf:
         """Load dict_cfg with tables to assign championship and rating points"""
 
         # Tables to assign points
@@ -122,3 +90,36 @@ class Configuration:
         )
 
         return extra_cfg
+
+
+class ConfigManager:
+    _current_config = None
+    _available_configs: List[Configuration] = None
+
+    def __init__(self) -> None:
+        pass
+
+    def initialize(self) -> None:
+        if ConfigManager._available_configs is None:
+            self.set_available_configs()
+
+    def set_available_configs(self) -> None:
+        ConfigManager._available_configs = []
+        for path in sorted(AVAILABLE_CONFIGS, reverse=True):
+            ConfigManager._available_configs.append(Configuration(path))
+            logger.debug("~ Available %s", ConfigManager._available_configs[-1])
+
+    def get_valid_config(self, date: str) -> OmegaConf:
+        self.initialize()
+        for conf in ConfigManager._available_configs:
+            if conf.start_valid_date <= date <= conf.end_valid_date:
+                return conf.get_config()
+
+    @property
+    def current_config(self) -> OmegaConf:
+        return ConfigManager._current_config
+
+    def set_current_config(self, date: str) -> None:
+        ConfigManager._current_config = self.get_valid_config(date)
+        if not os.path.exists(ConfigManager._current_config.io.data_folder):
+            os.mkdir(ConfigManager._current_config.io.data_folder)
